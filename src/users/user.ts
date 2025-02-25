d99e15f8322e587b8df67c7d023089e67015e6c4
WilliamKau12 <kau.william12@gmail.com>
2025-02-25 17:00:55 +0100

    update

--- src/users/user.ts
+++ src/users/user.ts
@@ -1,38 +1,56 @@
-import bodyParser from "body-parser";
 import express from "express";
 import { BASE_USER_PORT } from "../config";
-
-
 
 export type SendMessageBody = {
   message: string;
   destinationUserId: number;
 };
 
+export type MessageBody = {
+  message: string;
+};
+
+// Global state to store the last received message
 let lastReceivedMessage: string | null = null;
 let lastSentMessage: string | null = null;
 
 export async function user(userId: number) {
   const _user = express();
-  _user.use(express.json());
-  _user.use(bodyParser.json());
+  _user.use(express.json()); // Middleware to parse JSON request bodies
 
-  // TODO implement the status route
-  // _user.get("/status", (req, res) => {});
+  // Route to check the status of the user
   _user.get("/status", (req, res) => {
     res.send("live");
   });
 
-  // Implementing the /getLastReceivedMessage route
+  // Route to get the last received message
   _user.get("/getLastReceivedMessage", (req, res) => {
     res.json({ result: lastReceivedMessage });
   });
 
-  // Implementing the /getLastSentMessage route
+    // Route to get the last received message
   _user.get("/getLastSentMessage", (req, res) => {
     res.json({ result: lastSentMessage });
   });
 
+
+  // Route to receive messages
+  _user.post("/message", (req, res) => {
+    const { message }: MessageBody = req.body;
+
+    // Validate the request body
+    if (!message) {
+      return res.status(400).send("Message is required");
+    }
+
+    // Update the last received message
+    lastReceivedMessage = message;
+
+    // Return a plain text success response
+    return res.send("success");
+  });
+
+  // Start the server
   const server = _user.listen(BASE_USER_PORT + userId, () => {
     console.log(
       `User ${userId} is listening on port ${BASE_USER_PORT + userId}`
